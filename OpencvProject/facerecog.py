35b9d1946c3c5b935ea8ebdacc488143171cbcc8
as-bestinclass <as.bestinclass@gmail.com>
2023-07-10 22:11:25 -0500

    updated

--- OpencvProject/facerecog.py
+++ OpencvProject/facerecog.py
@@ -1,24 +1,21 @@
 import cv2
 
-
 faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
 eyeCascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-
 smileCascade = cv2.CascadeClassifier('haarcascade_smile.xml')
 
+# Specify the camera index (e.g., 0 for the laptop camera)
+camera_index = 1
 
-
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(camera_index)
 
 while True:
+    ret, img = cap.read()
+    cv2.imshow('Press ESC key to close this window', img)
+    k = cv2.waitKey(30)
 
-	ret, img = cap.read()
+    if k == 27:  # press 'ESC' to quit
+        break
 
-	cv2.imshow('video', img)
-
-	k = cv2.waitKey(30)
-
-	if k == 27: # press 'ESC' to quit
-
-		break
+cap.release()
+cv2.destroyAllWindows()
